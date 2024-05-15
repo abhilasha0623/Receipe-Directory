3e25df697bd96245a8949fec3dc747df7a2c223a
abhilasha.92.kp@gmail.com <abhilasha.92.kp@gmail.com>
2024-05-14 21:50:09 -0300

    fetchApi

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,20 @@
 import "../home/Home.css";
-
+import useFetch from "../../hooks/useFetch";
+import receipe from "../../../data/db.json";
 
 const Home =()=>{
+    const{data,isPending,error} = useFetch("http://localhost:3000/recipes")
+
+    console.log(receipe)
     return(
-        <div>Home</div>
+        <div className="home">
+            {error && <p className="error">{error}</p>}
+            {isPending && <p className="loading">Loading....</p>}
+            {data && data.map((receipe)=>{
+               return( <h2 key={receipe.id}>{receipe.title}</h2>);
+            })}
+
+        </div>
     )
 }
 
